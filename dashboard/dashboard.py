b2e0410eed2e92d371c2959047c90c220ee2f96c
afrizalrizqi <afrizalrizqi02@gmail.com>
2023-11-29 13:12:58 +0700

    first commit

---
+++ dashboard/dashboard.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import streamlit as st
+
+#Load dataset
+
+df = pd.read_csv("https://raw.githubusercontent.com/fikrionii/Dicoding-Bike-Sharing/main/dataset/cleaned_bikeshare_hour.csv")
